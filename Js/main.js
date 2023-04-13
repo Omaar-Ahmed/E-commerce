e8deedf3dcf17804c7b09f45dc907dd6af074073
Omaar-Ahmed <127250628+Omaar-Ahmed@users.noreply.github.com>
2023-04-13 17:55:52 +0200

    Update main.js

---
+++ Js/main.js
@@ -0,0 +1,14 @@
+let menuBars = document.getElementById('bars');
+let linksNavBar = document.getElementById('links');
+menuBars.onclick = () => {
+    menuBars.classList.toggle('active');
+    linksNavBar.classList.toggle('show');
+}
+
+document.addEventListener('click', function (e) {
+    console.log(e.target.id)
+    if (e.target.id !== 'bars' && e.target.id !== 'links') {
+        menuBars.classList.remove('active');
+        linksNavBar.classList.remove('show');
+    }
+})

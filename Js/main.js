e65ebf388621e40dd2323c6f37e797bffc0390f5
Omaar-Ahmed <127250628+Omaar-Ahmed@users.noreply.github.com>
2023-04-25 02:02:25 +0200

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -4,11 +4,25 @@ menuBars.addEventListener("click", function(){
     menuBars.classList.toggle('active');
     linksNavBar.classList.toggle('show');
 })
-
-
 document.addEventListener('click', function (e) {
     if (!linksNavBar.contains(e.target)&&!menuBars.contains(e.target)) {
         menuBars.classList.remove('active');
         linksNavBar.classList.remove('show');
     }
 })
+// Single Product Page ------------------------
+let mainImg = document.getElementById('mainImg'); 
+let smallImgs = document.getElementsByClassName('small-img') ; 
+
+for (let i=0 ; i < smallImgs.length ; i++) {
+smallImgs[i].onclick = function() {
+    mainImg.src = smallImgs[i].src ;
+}}
+
+
+let products = document.getElementsByClassName('boxOpen');
+console.log(products)
+for (let i=0 ; i < products.length ; i++) {
+products[i].onclick = function() {
+    window.location.href= 'sproduct.html';
+}}
